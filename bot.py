4e51788b60e6f9da8f9f71169f4aaa97655de6f3
justatest420 <justatestsubject.01@gmail.com>
2025-01-28 14:11:46 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@
 
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
-
+"""
 class Bot(Client):
 
     def __init__(self):
@@ -28,8 +28,18 @@ class Bot(Client):
 
         await super().stop()
         print('Bot Stopped Bye')
+"""
+Bot = Client(
+    "techvj login",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="TechVJ"),
+    workers=50,
+    sleep_threshold=10) 
 
-Bot().run()
+
+Bot.run()
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
